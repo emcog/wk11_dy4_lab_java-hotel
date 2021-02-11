d40439bd02071d2f8f16279c9af84d6f9386c4e2
emcog <robin.leonard@gmail.com>
2021-02-11 15:37:27 +0000

    basic hotel tests passing

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,12 +1,33 @@
+import java.util.ArrayList;
+
 public class Hotel {
 
     private String hotelName;
+    private int setNumberOfBedRooms;
+    private ArrayList<Bedroom> bedRooms;
 
-    public Hotel(String hotelName){
+    public Hotel(String hotelName, int setNumberOfBedRooms) {
         this.hotelName = hotelName;
+        this.setNumberOfBedRooms = setNumberOfBedRooms;
+        this.bedRooms = new ArrayList<Bedroom>();
     }
 
     public String getHotelName() {
         return this.hotelName;
     }
+
+    public int getSetNumberOfBedRooms() {
+        return this.setNumberOfBedRooms;
+    }
+
+    public int getBedroomCount(){
+        return this.bedRooms.size();
+    }
+
+    public void addBedroom(Bedroom bedroom) {
+        if (bedRooms.size() < setNumberOfBedRooms) {
+            this.bedRooms.add(bedroom);
+        }
+    }
 }
+
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -6,18 +6,41 @@ import static org.junit.Assert.assertEquals;
 public class HotelTest {
 
     Hotel hotel;
+    Bedroom loveShack;
 
     @Before
     public void before(){
-        hotel = new Hotel("California");
+        hotel = new Hotel("California", 6);
+        loveShack = new Bedroom(1, BedRoomType.TRIPLE);
     }
 
     @Test
     public void hotelHasName(){
         assertEquals("California", hotel.getHotelName());
     }
-//    hasname
-//    has bedrooms
-//    has conference rooms
 
+    @Test
+    public void hotelCanHaveBedrooms(){
+        assertEquals(6, hotel.getSetNumberOfBedRooms());
+    }
+
+    @Test
+    public void hotelCanAddABedroom(){
+        hotel.addBedroom(loveShack);
+        assertEquals(1, hotel.getBedroomCount());
+    }
+
+    @Test
+    public void hotelCanNotExceedSetNumberOfBedrooms(){
+            hotel.addBedroom(loveShack);
+            hotel.addBedroom(loveShack);
+            hotel.addBedroom(loveShack);
+            hotel.addBedroom(loveShack);
+            hotel.addBedroom(loveShack);
+            hotel.addBedroom(loveShack);
+            hotel.addBedroom(loveShack);
+
+            assertEquals(6, hotel.getBedroomCount());
+
+    }
 }
