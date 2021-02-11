53c8ddf9b5efdf61d3ad863bf838d59f77ca3540
emcog <robin.leonard@gmail.com>
2021-02-11 11:31:39 +0000

    first test passed

---
+++ src/main/java/Guest.java
@@ -0,0 +1,17 @@
+public class Guest {
+
+    private String guestName;
+
+    public Guest(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getName() {
+        return this.guestName;
+    }
+
+//    public String setName() {
+//        this.guestName = guestName;
+//    }
+
+}
